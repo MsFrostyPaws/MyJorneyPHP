090f13e83dbfc48cb6b03333ac7045f739ac55ac
Anna Lepneva <100154307+MsFrostyPaws@users.noreply.github.com>
2022-03-03 14:17:00 +0300

    Create CountSaple.php

---
+++ CountSaple.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$items = [
+	['sort' => 1],
+    ['sort' => 3],
+    ['sort' => 5],
+    ['name' => 'items 1']
+];
+
+$sum = 0;
+foreach ($items as $item)  {
+	$sum+=$item['sort'];
+}
+
+echo $sum;
+?>
+
+</body>
+</html>
